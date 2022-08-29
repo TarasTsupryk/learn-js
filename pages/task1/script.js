dd146f4f2b049574abfae9afa60ddcfea63084f6
YuhneiNazar <yuhnei.nazar@gmail.com>
2022-08-29 12:08:37 +0300

    changetext

---
+++ pages/task1/script.js
@@ -0,0 +1,3 @@
+function changeText() {
+ document.getElementById('text__container').innerText = 'Work is the best way to love life. Do what you were born to do. It is better to work without a definite goal than to do nothing. Laziness makes any work difficult, and diligence makes it easy.';
+}
