707849d5dd0a334a4ee7e8c32c9104631c4c0f68
Taras Tsupryk <taras.tsupryk@intellias.com>
2022-08-31 15:35:50 +0300

    add constants

---
+++ src/common/constants.js
@@ -0,0 +1,11 @@
+// date formats
+export const DEFAULT_DATE_FORMAT = "MM/DD/YYYY";
+export const YTD_DATE_FORMAT = "DD-MMM-YYYY";
+export const SHORT_DATE_FORMAT = "DD/MM/YY";
+export const DATE_MASK_TIME = "DD MMM YYYY _ h:mm A";
+export const DATE_MASK_TIME_WITHOUT_YEAR = "DD MMM _ h:mm a";
+export const MINIMIZE_DATE_FORMAT = "MM/DD/YY";
+export const DEFAULT_DATE_MASK = "99/99/9999";
+export const DEFAULT_INPUT_DATE_FORMAT = "DD/MM/YYYY";
+export const RAW_DATE_FORMAT = "YYYY-MM-DD";
+export const LONG_RAW_DATE_FORMAT = "YYYY-MM-DDTHH:mm:ss.ss[Z]";
